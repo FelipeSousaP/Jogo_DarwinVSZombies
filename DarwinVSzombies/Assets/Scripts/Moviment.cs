fb237abff7df0318e0f5b95f34c89325ce3c2332
FelipeSousaP <comffelipe@gmail.com>
2026-03-09 17:30:51 -0300

    FEAT: adicionando movimento

---
+++ DarwinVSzombies/Assets/Scripts/Moviment.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Moviment : MonoBehaviour
+{
+    [SerializeField] InputActionReference MoveAction;
+    [SerializeField] Transform Player;
+    [SerializeField] float Speed;
+    Vector2 dir;
+
+    private void OnEnable()
+    {
+        if(MoveAction != null)
+        {
+            MoveAction.action.performed += Move;
+            MoveAction.action.canceled += Move;
+        }
+    }
+    private void OnDisable()
+    {
+        MoveAction.action.performed -= Move;
+        MoveAction.action.canceled -= Move;
+    }
+
+    void Move(InputAction.CallbackContext callbackContext)
+    {
+        dir = callbackContext.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+        Player.position += new Vector3(dir.x, dir.y,0) * Speed * Time.deltaTime;
+    }
+}
